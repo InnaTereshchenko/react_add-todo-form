1a861cd511c949892c7b25323e9b8076e8d36aa3
Inna <inna.tereshchenko1@gmail.com>
2019-12-11 14:37:30 +0200

    Updates

---
+++ precache-manifest.0cea4df68ff0150c836fef74321a73f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a95e9014d3a9628d7fc58f9e11f46cf4",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "21c5bd13fe0235ff1cc5",
+    "url": "/react_add-todo-form/static/css/main.86121a56.chunk.css"
+  },
+  {
+    "revision": "710e1266e890094367a4",
+    "url": "/react_add-todo-form/static/js/2.36d60507.chunk.js"
+  },
+  {
+    "revision": "21c5bd13fe0235ff1cc5",
+    "url": "/react_add-todo-form/static/js/main.76e773e4.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
